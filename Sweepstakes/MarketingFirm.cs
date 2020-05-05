c7a284bea57534a6c3fb0394496260255b61bd27
Fowley <alexanderfowle@gmail.com>
2020-05-05 13:20:12 -0500

    Added comment pointing out example of DI and factory design implementation

--- Sweepstakes/MarketingFirm.cs
+++ Sweepstakes/MarketingFirm.cs
@@ -15,6 +15,16 @@ namespace Sweepstakes
             _manager = manager;
         }
 
+        // Dependency Injection (DI) is being used in the MarketingFirm constructor to 
+        // assign _manager to whichever Manager class the user chooses from the Simulation
+        // class. Since both Manager classes inherit from the ISweepstakesManager interface,
+        // methods within the child classes are invoked in the same way (though the logic
+        // within those member methods differ in each class). Therefore, by implementing DI
+        // following the factory design pattern, this application is much more flexible 
+        // than if the "new" keyword would have been used to hardcode which child class of
+        // the ISweepstakesManager interface needs to be used on instatiation of a 
+        // MarketingFirm() object.
+
         // Methods
         public void CreateSweepstake()
         {
